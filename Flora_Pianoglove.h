7e4a0fda16250baa61a42acd5f71d37e911cb918
ellaholl <72042842+ellaholl@users.noreply.github.com>
2023-11-05 11:10:26 -0500

    files from adafruit tutorial

---
+++ Flora_Pianoglove.h
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2019 Anne Barela for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+typedef struct RgbColor
+{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+} RgbColor;
+
+typedef struct HsvColor
+{
+    unsigned char h;
+    unsigned char s;
+    unsigned char v;
+} HsvColor;
